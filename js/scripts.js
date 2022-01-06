3224be2ac4f8935d06e53de4692379c2855f20d2
rhemney <40870441+rhemney@users.noreply.github.com>
2022-01-06 14:10:51 +0300

    Updated Journal Page

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,3 @@
-/*!
-* Start Bootstrap - Creative v7.0.3 (https://startbootstrap.com/theme/creative)
-* Copyright 2013-2021 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-creative/blob/master/LICENSE)
-*/
-//
-// Scripts
-// 
 
 window.addEventListener('DOMContentLoaded', event => {
 
